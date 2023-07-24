bcd28ae00e0ddf83f589778272343022fda57c26
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-24 13:13:02 +0100

    feat: set up custom fonts in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: [
+          "'Neue Haas Grotesk Display Pro', sans-serif",
+          ...fontFamily.sans,
+        ],
+        heading: ["Padaloma", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
