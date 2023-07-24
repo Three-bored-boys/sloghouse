6e62c9a53899fb69fddc76be5f97a93e00a31b3a
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-24 10:48:32 +0100

    feat: set up astro image tools to work with our project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroImageTools],
 });
