2f76fee6ff26896dc30f758ce4a9abd13ba85873
Kelechiodom10 <kelechiodom10@gmail.com>
2023-07-29 20:45:26 +0100

    feat: create projects section with nice animations

---
+++ src/components/projects/data.ts
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    id: "1",
+    name: "Slog House",
+    description: "A house in the woods",
+    image: "/assets/images/sloghouse.jpeg",
+  },
+  {
+    id: "2",
+    name: "Slog House 1",
+    description: "Some other house in the woods",
+    image: "/assets/images/sloghouse-1.jpeg",
+  },
+  {
+    id: "3",
+    name: "Slog House 2",
+    description: "Another house in the woods",
+    image: "/assets/images/sloghouse-2.jpeg",
+  },
+  // repeat
+  {
+    id: "1",
+    name: "Slog House",
+    description: "A house in the woods",
+    image: "/assets/images/sloghouse.jpeg",
+  },
+  {
+    id: "2",
+    name: "Slog House 1",
+    description: "Some other house in the woods",
+    image: "/assets/images/sloghouse-1.jpeg",
+  },
+  {
+    id: "3",
+    name: "Slog House 2",
+    description: "Another house in the woods",
+    image: "/assets/images/sloghouse-2.jpeg",
+  },
+];
